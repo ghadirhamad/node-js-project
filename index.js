cc4a185653ee132fdc8bfad426a50a9264aefaeb
غدير حمد <gogo14hamad@gmail.com>
2025-07-04 06:05:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,10 +44,11 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://node-js-project-gael.onrender.com/auth/google/callback"
 }, (accessToken, refreshToken, profile, done) => {
   done(null, profile);
 }));
+
 
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
